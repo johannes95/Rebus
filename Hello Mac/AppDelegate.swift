cfe089e4ca9b54180d7993b1668efb7980df07e7
Johannes Flood <johannesflood@Johanness-MacBook-Pro.local>
2015-11-22 13:41:39 +0100

    Error
    Not working

---
+++ Hello Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hello Mac
+//
+//  Created by Johannes Flood on 2015-11-22.
+//  Copyright © 2015 Johannes Flood. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hello Mac/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Hello Mac
+//
+//  Created by Johannes Flood on 2015-11-22.
+//  Copyright © 2015 Johannes Flood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        @IBOutlet weak var playBtn: UIButton!
+        @IBOutlet weak var playBtn: UIButton!
+        @IBOutlet weak var playBtn: UIButton!
+        @IBOutlet weak var playBtn: UIButton!
+        @IBOutlet weak var playBtn: UIButton!
+        @IBOutlet weak var playButton: UIButton!
+        didSet {
+        // Update the view, if already loaded.
+        }
+        @IBAction func playBtnPressed(sender: AnyObject) {
+        }
+    }
+
+
+}
+
+@IBOutlet weak var c: UIButton!
